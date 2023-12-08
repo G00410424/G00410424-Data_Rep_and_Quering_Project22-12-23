8c76e979106d5e26cee1ed424e2c3ea519944dad
g00410424@atu.ie <g00410424@atu.ie>
2023-12-08 17:26:24 +0000

    Creating and changing editTrip.js

---
+++ src/components/editTrip.js
@@ -0,0 +1,86 @@
+import { useParams, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function Edit() {
+    let {id} = useParams();
+
+    const [location, setLocation] = useState('');
+    const [cover, setCover] = useState('');
+    const [description, setDescription] = useState('');
+
+    const navigate = useNavigate();
+
+    useEffect(
+        ()=>{
+
+            axios.get('http://localhost:4000/api/trip/'+id)
+            .then((response)=>{
+                setLocation(response.data.location);
+                setCover(response.data.cover);
+                setDescription(response.data.description);
+            })
+            .catch(
+                (error)=>{
+                    console.log(error);
+                }
+            );
+        },[]
+    );
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+
+        const book = {
+            location:location,
+            cover:cover,
+           description:description
+        }
+
+        axios.put('http://localhost:4000/api/trip/'+id, trip)
+        .then((res)=>{
+            navigate('/read');
+        })
+        .catch(
+            (error)=>{
+                console.log(error)
+            });
+    }
+    return (
+        <div>
+            <h2>Hello from Edit component!</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>Edit Trip Location: </label>
+                    <input type="text"
+                        className="form-control"
+                        value={location}
+                        onChange={(e) => { setLocation(e.target.value) }}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Edit Trip Cover: </label>
+                    <input type="text"
+                        className="form-control"
+                        value={cover}
+                        onChange={(e) => { setCover(e.target.value) }}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Edit Book Author: </label>
+                    <input type="text"
+                        className="form-control"
+                        value={author}
+                        onChange={(e) => { setAuthor(e.target.value) }}
+                    />
+                </div>
+                <div>
+                    <input type="submit"
+                    value="Edit Book">
+                        </input>
+                </div>
+            </form>
+
+        </div>
+    );
+}
