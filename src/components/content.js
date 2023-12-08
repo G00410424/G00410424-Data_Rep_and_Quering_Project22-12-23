f6b6064955930bd9332cf409954ebd40c4aeb61e
g00410424@atu.ie <g00410424@atu.ie>
2023-12-08 16:25:28 +0000

    First component added

---
+++ src/components/content.js
@@ -0,0 +1,9 @@
+function Content() {
+    return (
+        <div>
+            <h1>Hello World, this are my trips!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+}
+export default Content;
