34f37b8a4c43cdacfe63974238f3fb47f159edc9
g00410424@atu.ie <g00410424@atu.ie>
2023-12-09 11:45:58 +0000

    adding first image

--- src/components/content.js
+++ src/components/content.js
@@ -4,8 +4,8 @@ function Content() {
             <h1>Hello World, these are my trips!</h1>
             <h2>It is {new Date().toLocaleTimeString()}.</h2>
             <img
-                src="/images/your-image.jpg"
-                alt="Description of the image"
+                src="/Prague.png"
+                alt="Prague"
                 style={{ width: "100%", maxWidth: "500px", height: "auto" }}
             />
         </div>
