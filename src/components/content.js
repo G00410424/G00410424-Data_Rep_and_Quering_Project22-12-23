470d309cb55f508fd6c08c7e9e990bd1db7f4977
g00410424@atu.ie <g00410424@atu.ie>
2023-12-09 12:43:48 +0000

    Adding first link to my images

--- src/components/content.js
+++ src/components/content.js
@@ -1,31 +1,35 @@
 function Content() {
     const imageStyle = {
         width: "100%",
-        maxWidth: "200px",
-        height: "200",
+        maxWidth: "633px",
+        height: "300px",
         margin: "1px",
         border: "5px solid black", // Add border style
+        transition: "transform 0.2s", // Add a smooth transition effect
     };
 
     const gridContainerStyle = {
         display: "grid",
         gridTemplateColumns: "repeat(3, 1fr)",
-        gap: "1px", // Reduce the gap between images
+        gap: "2px", // Reduce the gap between images
         justifyContent: "center", // Center the container horizontally
     };
+
 
     return (
         <div>
             <h1>Hello World, these are my trips!</h1>
             <h2>It is {new Date().toLocaleTimeString()}.</h2>
+            <br></br>
 
             <div style={gridContainerStyle}>
+            <a href="https://en.wikipedia.org/wiki/Prague" target="_blank" rel="noopener noreferrer">
                 <img
                     src="/Prague.png"
                     alt="Prague"
                     style={imageStyle}
                 />
-
+            </a>
                 <img
                     src="/MachuPicchu.jpg"
                     alt="Machu Picchu"
