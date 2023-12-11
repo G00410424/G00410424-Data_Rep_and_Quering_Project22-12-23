c1c77cb61a57e763499908ab321b9363dddbf63f
g00410424@atu.ie <g00410424@atu.ie>
2023-12-11 15:10:11 +0000

    Added comments to editTrip.js

--- src/components/editTrip.js
+++ src/components/editTrip.js
@@ -1,18 +1,24 @@
+// Import necessary modules
 import { useParams, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { footerStyle, Footer } from './footer'; // Adjust the path based on your project structure
+import { footerStyle, Footer } from './footer'; 
 
+// React functional component for editing a trip
 export default function Edit() {
+    // Get the id parameter from the URL using React Router's useParams
     let {id} = useParams();
 
+     // State variables for form input fields
     const [location, setLocation] = useState('');
     const [cover, setCover] = useState('');
     const [description, setDescription] = useState('');
     const [date, setDate] = useState('');
 
+     // Access the navigation function from React Router
     const navigate = useNavigate();
 
+    // Styles for various containers
     const contentContainerStyle = {
         display: "flex", // Use flexbox to arrange content
         padding: "20px",
@@ -30,6 +36,7 @@ export default function Edit() {
         textAlign: "left",
     };
     
+     // Use useEffect to fetch the trip data based on the id when the component mounts
     useEffect(
         ()=>{
             axios.get('http://localhost:4000/api/trip/'+id)
@@ -45,12 +52,14 @@ export default function Edit() {
                     console.log(error);
                 }
             );
-        },[]
+        },[id] // Include id as a dependency to re-fetch data when id changes
     );
-
+    
+    // Function to handle form submission
     const handleSubmit = (e)=>{
         e.preventDefault();
 
+        // Create trip object with updated form values
         const trip = {
             location:location,
             cover:cover,
@@ -58,8 +67,10 @@ export default function Edit() {
            date:date
         }
 
+        // Use Axios to make a PUT request to update the trip
         axios.put('http://localhost:4000/api/trip/'+id, trip) 
         .then((res)=>{
+            // Navigate to the 'read' page after successful submission
             navigate('/read');
         })
         .catch(
@@ -67,10 +78,13 @@ export default function Edit() {
                 console.log(error)
             });
     }
+    // Return JSX for the component
     return (
         <div >
             <h2>LETS EDIT OUR TRIPS</h2>
+             {/* Form for editing a trip */}
             <form onSubmit={handleSubmit}>
+                {/* Form input fields */}
                 <div className="form-group" style={{ display: "block", textAlign: "left", marginTop: "2rem"}}>
                     <label>Edit Trip Location: </label>
                     <input type="text"
@@ -80,7 +94,7 @@ export default function Edit() {
                         onChange={(e) => { setLocation(e.target.value) }}
                     />
                 </div>
-
+                {/* Repeat similar blocks for other input fields */}
                 <div className="form-group" style={{ display: "block", textAlign: "left", marginTop: "2rem"}}>
                     <label>Edit Trip Cover: </label>
                     <input type="text"
@@ -109,12 +123,14 @@ export default function Edit() {
                         onChange={(e) => { setDescription(e.target.value) }}
                     />
                 </div>
+                 {/* Submit button */}
                 <div style={{ textAlign: "left", marginTop: "1rem"}}>
                     <input type="submit"
                     value="Edit Trip">
                         </input>
                 </div>
             </form>
+            {/* Include the Footer component */}
             <Footer />
         </div>
     );
