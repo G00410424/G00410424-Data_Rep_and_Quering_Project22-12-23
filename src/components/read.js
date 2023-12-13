2a34a1b4e03d68e5b7dc0358453907cb7027da85
g00410424@atu.ie <g00410424@atu.ie>
2023-12-13 20:50:55 +0000

    more comments

--- src/components/read.js
+++ src/components/read.js
@@ -1,15 +1,19 @@
-// Read.js
+// Read.js - This file contains the code for the Read component, which displays a list of trips.
 
+// Import necessary modules
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Trips from "./trips";
 import Search from "./search";
-import { footerStyle, Footer } from './footer'; // Adjust the path based on your project structure
+import { footerStyle, Footer } from './footer'; 
 
+// Functional component representing the Read page
 function Read() {
+  // State to store the fetched data and filtered data for trips
   const [data, setData] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
 
+    // Fetch data from the API when the component mounts
   useEffect(() => {
     axios.get("http://localhost:4000/api/trips")
       .then((response) => {
@@ -20,6 +24,7 @@ function Read() {
       });
   }, []);
 
+  // Function to reload data by making a new API request
   const reload = () => {
     axios.get("http://localhost:4000/api/trips")
       .then((response) => {
@@ -30,6 +35,7 @@ function Read() {
       });
   };
 
+  // Function to handle search input and filter trips based on location
   const handleSearch = (searchTerm) => {
     const filteredTrips = data.filter((trip) =>
       trip.location.toLowerCase().includes(searchTerm.toLowerCase())
@@ -37,14 +43,17 @@ function Read() {
     setFilteredData(filteredTrips);
   };
 
+  // Render the Read component
   return (
     <div style={{ position: "relative" }}>
       <h2>Trips List</h2>
       <Search onSearch={handleSearch} />
       <Trips myTrips={filteredData.length > 0 ? filteredData : data} ReloadData={reload} />
+      {/* Include the Footer component */}
       <Footer />
     </div>
   );
 }
 
+// Exporting the Read component for use in other files
 export default Read;
--- src/components/search.js
+++ src/components/search.js
@@ -1,7 +1,9 @@
-// Search.js
+// Search.js - to search for specific trips on the read page.
 
+// Import necessary modules
 import React, { useState } from "react";
 
+// Styling for the search container
 const searchContainerStyle = {
   position: "absolute",
   top: "10px",
@@ -9,13 +11,17 @@ const searchContainerStyle = {
   textAlign: "right",
 };
 
+// Functional component representing the search input and button
 function Search({ onSearch }) {
+    // State to store the search term entered by the user
   const [searchTerm, setSearchTerm] = useState("");
 
+  // Function to handle the search and invoke the onSearch callback
   const handleSearch = () => {
     onSearch(searchTerm);
   };
 
+  // Render the Search component
   return (
     <div style={searchContainerStyle}>
       <input
@@ -29,4 +35,5 @@ function Search({ onSearch }) {
   );
 }
 
+// Exporting the Search component for use in other files
 export default Search;
